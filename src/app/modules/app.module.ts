8387d5126d716d277c77692044181fbbd5a1c3ab
Monster-Mohamed <ma8494927@gmail.com>
2022-10-02 10:35:03 +0200

    new update v2.3 * modified app.module

--- src/app/modules/app.module.ts
+++ src/app/modules/app.module.ts
@@ -1,13 +1,17 @@
-import { Module } from "@nestjs/common";
-import { AppController } from "@app/app/controllers/app.controller";
-import { AppService } from "@app/app/services/app.service";
-import { TypeOrmModule } from "@nestjs/typeorm";
-import ormConfig from "@app/ormconfig";
-import { ConfigModule } from "@nestjs/config";
+import { Module } from '@nestjs/common';
+import { AppController } from '@app/app/controllers/app.controller';
+import { AppService } from '@app/app/services/app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import ormConfig from '@app/ormconfig';
+import { ConfigModule } from '@nestjs/config';
 import { TestModule } from './test.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(ormConfig), ConfigModule.forRoot([TestModule])],
+  imports: [
+    TypeOrmModule.forRoot(ormConfig),
+    ConfigModule.forRoot(),
+    TestModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
